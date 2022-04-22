324b6351022135adc748be80478a9c865471a0ca
Anita Liberatore <anitaliberatore@hotmail.com>
2022-04-22 18:39:24 +0200

    add button component and header component

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+ <button :style="{ background: color }" class="btn">{{label}}</button>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    label: String,
+    color: String
+  }
+}
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button label="Add Task" color="blue"/>
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  props: {
+    title: String
+  },
+  components: {
+    Button
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
