75c1601e914335b7b158f1c5f6098f8b843453c2
Anita Liberatore <anitaliberatore@hotmail.com>
2022-04-22 13:02:01 +0200

    clean components unused

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,7 @@
 <script setup>
 defineProps({
   msg: {
-    type: String,
-    required: true
+    type: String
   }
 })
 </script>
@@ -10,11 +9,6 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
   </div>
 </template>
 
